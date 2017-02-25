bb7e0d89748e7d3f13e67eed511d45e399dea9a1
Mohammed F Omar <mohammed.f.omar@gmail.com>
2017-02-25 10:23:18 +0000

    Basic CRUD

---
+++ src/main/java/example/PersonRepository.java
@@ -0,0 +1,9 @@
+package example;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
